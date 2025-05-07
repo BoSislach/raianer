58b7db8ab3232401871f7be24f363d9431df8aa7
Gabrielit018 <gabriele.zampis@allievi.bearzi.it>
2025-05-07 22:03:57 +0200

    Update Aereo.java

--- Aereo.java
+++ Aereo.java
@@ -1,9 +1,11 @@
+import java.util.Random;
+
 public class Aereo extends Thread {
     private String codiceVolo;
     private String costruttore;
     private int maxPasseggeri;
     private int passeggeri;
-    private int maxBagagli;
+    private int pesoMaxBagagli;
     private int bagagli;
     private boolean solaAndata;
 
@@ -11,22 +13,23 @@ public class Aereo extends Thread {
     private AreaSosta areaSosta;
     private Hangar hangar;
 
-    public Aereo(String codiceVolo, String costruttore, int maxPasseggeri, int maxBagagli, boolean solaAndata,
+    public Aereo(String codiceVolo, String costruttore, int maxPasseggeri, int pesoMaxBagagli, boolean solaAndata,
                  Pista[] piste, AreaSosta areaSosta, Hangar hangar) {
         this.codiceVolo = codiceVolo;
         this.costruttore = costruttore;
         this.maxPasseggeri = maxPasseggeri;
-        this.maxBagagli = maxBagagli;
+        this.pesoMaxBagagli = pesoMaxBagagli;
         this.solaAndata = solaAndata;
         this.piste = piste;
         this.areaSosta = areaSosta;
         this.hangar = hangar;
     }
-
+    //getter per identificare l'aereo 
     public String getCodiceVolo() {
         return codiceVolo;
     }
-
+    // il vero e proprio codice del thread, contiene tutte le azione e le condizioni che l'aereo fa 
+    // è chiamato run perchè è la keyword per far eseguire al thread questo determinato codice quando lo chiamimao nel main 
     public void run() {
         hangar.esci(this);
         areaSosta.entra(this);
@@ -35,14 +38,15 @@ public class Aereo extends Thread {
         VeicoloBagagli.caricaBagagli(this);
 
         passeggeri = maxPasseggeri;
-        bagagli = maxBagagli;
+        bagagli = pesoMaxBagagli;
 
         areaSosta.esci(this);
         usaPista("Decollo");
 
-        // Simula volo
-        try { Thread.sleep(2000); } catch (InterruptedException ignored) {}
-
+        // bisogna per forza  utilizzare un eccezzione per catturare l'errore in caso avvega 
+        try { Thread.sleep(2000); }
+         catch (InterruptedException ignored) {}
+        
         usaPista("Atterraggio");
 
         areaSosta.entra(this);
@@ -53,7 +57,7 @@ public class Aereo extends Thread {
             hangar.entra(this);
         }
     }
-
+    // questo metodo lavora col semaforo della pista per consentire l'accesso a solamente 3 aerei in concomitanza 
     private void usaPista(String azione) {
         for (Pista pista : piste) {
             pista.usa(this, azione);
